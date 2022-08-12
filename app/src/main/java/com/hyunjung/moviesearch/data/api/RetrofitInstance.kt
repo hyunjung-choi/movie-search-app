4246164741575336970d611921bb8c61192030e0
hyunjung-choi <hjchoi.dev@gmail.com>
2022-08-12 17:38:22 +0900

    [feat] Retrofit 객체 생성

---
+++ app/src/main/java/com/hyunjung/moviesearch/data/api/RetrofitInstance.kt
@@ -0,0 +1,29 @@
+package com.hyunjung.moviesearch.data.api
+
+import com.hyunjung.moviesearch.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitInstance {
+    private val okHttpClient: OkHttpClient by lazy {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    val api: MovieSearchApi by lazy {
+        retrofit.create(MovieSearchApi::class.java)
+    }
+}
---
+++ app/src/main/java/com/hyunjung/moviesearch/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.hyunjung.moviesearch.util
+
+import com.hyunjung.moviesearch.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://openapi.naver.com/"
+    const val CLIENT_ID = BuildConfig.clientId
+    const val CLIENT_SECRET = BuildConfig.clientSecret
+}
